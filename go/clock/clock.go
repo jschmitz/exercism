9224fc5984d71b4a44925c5005f25a0cc285b90d
Jake Schmitz <jschmitz@gmail.com>
2017-01-18 09:03:40 -0600

    Update for Go Clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,66 +1,36 @@
-// Clock stub file
-
-// To use the right term, this is the package *clause*.
-// You can document general stuff about the package here if you like.
+// Package clock implements a Clock with continous roll over
 package clock
 
 import (
 	"fmt"
 )
 
-const (
-	testVersion   = 4  //exercism test version
-	minutesInHour = 60 // number of minutes in a hour
-	hoursInDay    = 24 // number of hours in a day
-)
+const testVersion = 4
 
-// Clock API as stub definitions.  No, it doesn't compile yet.
-// More details and hints are in clock_test.go.
+type Clock int
 
-type Clock struct {
-	hour, minute int
-}
-
+// New accepts hour and minute ints and
+// converts to Clock minutes - (24 * 60) max
 func New(hour, minute int) Clock {
-	remainderMinutes, minuteHours := continousMinutes(minute)
-	h := continousHours(minuteHours + hour)
-	return Clock{hour: h, minute: remainderMinutes}
+	minutes := (hour*60 + minute)
+	return minutesToClock(minutes)
 }
 
+// String formats the Clock to hh:mm
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+	return fmt.Sprintf("%02d:%02d", c/60, c%60)
 }
 
+// Add updates Clock by minutes
 func (c Clock) Add(minutes int) Clock {
-	remainderMinutes, minuteHours := continousMinutes(c.minute + minutes)
-	h := continousHours(minuteHours + c.hour)
-	c.minute = remainderMinutes
-	c.hour = h
-	return c
+	return minutesToClock(int(c) + minutes)
 }
 
-func continousHours(hours int) int {
-	if hours < 0 {
-		hours = hoursInDay - (hours*-1)%hoursInDay
-		if hours == hoursInDay {
-			hours = 0
-		}
-	} else {
-		hours = hours % hoursInDay
-	}
-
-	return hours
-}
-
-func continousMinutes(minutes int) (int, int) {
-	var remainder, hours int
+func minutesToClock(minutes int) Clock {
+	minutes = minutes % (24 * 60)
 	if minutes < 0 {
-		remainder = minutesInHour - (minutes*-1)%minutesInHour
-		hours = (minutes / minutesInHour) - 1
-	} else {
-		remainder = minutes % minutesInHour
-		hours = minutes / minutesInHour
+		minutes += (24 * 60)
 	}
+	return Clock(minutes)
 
-	return remainder, hours
 }
