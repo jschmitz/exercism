e7d12daf328c692ab6ff30b9eef54c392fc17666
Jake Schmitz <jschmitz@vailsys.com>
2019-12-05 17:58:49 -0600

    Update bob solution

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,33 +1,23 @@
 defmodule Bob do
   def hey(input) do
-    if String.trim(input) == "" do
-      "Fine. Be that way!"
-    else
-
-      last_char = String.at(input, String.length(input) - 1)
-      punc? = String.match?(last_char, ~r/[?!.]/)
-      all_upper = input != String.upcase(input)
-      has_letters = String.match?(input, ~r/[\p{L}]/)
-
-      if punc? do
-        if has_letters do
-          result(last_char, all_upper)
-        else
-          "Sure."
-        end
-      else
-        if has_letters do
-          "Whoa, chill out!"
-        else
-          "Whatever."
-        end
-      end
+    cond do
+      nothing?(input) -> "Fine. Be that way!"
+      question?(input) && shouting?(input) -> "Calm down, I know what I'm doing!"
+      shouting?(input) ->  "Whoa, chill out!"
+      question?(input) ->  "Sure."
+      true -> "Whatever."
     end
   end
 
-  def result(p, caps?) when p == "." and caps?, do: "Whatever."
-  def result(p, caps?) when p == "!" and not caps?, do: "Whoa, chill out!"
-  def result(p, caps?) when p == "!" and caps?, do: "Whatever."
-  def result(p, caps?) when p == "?" and caps?, do: "Sure."
-  def result(p, caps?) when p == "?" and not caps?, do: "Calm down, I know what I'm doing!"
+  def nothing?(input) do
+    String.trim(input) == ""
+  end
+
+  def question?(input) do
+    String.last(input) == "?"
+  end
+
+  def shouting?(input) do
+    input == String.upcase(input) && String.match?(input, ~r/[\p{L}]/)
+  end 
 end
