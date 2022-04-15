42b9bb99556fa90ec48b021b6ae41cb1d6d4cdaa
Jake Schmitz <jschmitz@gmail.com>
2022-04-15 05:45:50 -0500

    Complete TypeScrpt resistor color duo

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,16 @@
+const resistors: { [key: string]: number } = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export function decodedValue(colors: Array<string>): number {
+  return parseInt(`${resistors[colors[0]]}${resistors[colors[1]]}`);
+}
