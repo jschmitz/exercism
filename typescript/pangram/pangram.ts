2829f675a458c23655a064c999d3de281f23f907
Jake Schmitz <jschmitz@gmail.com>
2022-04-26 07:12:08 -0500

    Complete pangram

---
+++ typescript/pangram/pangram.ts
@@ -0,0 +1,36 @@
+export function isPangram(characters: string): boolean {
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  const filtered = alphabet.filter(
+    (char) => !characters.toLowerCase().split("").includes(char)
+  );
+
+  return filtered.length === 0;
+}
